9c5629a11a516b692b036f6121707c9f3dde21dd
trickyBytes <richard.tiffin@ovo.com>
2023-12-04 19:53:33 +0000

    day001, part 2 complete

--- src/test/kotlin/com/akoolla/adventofcode/day001.kt
+++ src/test/kotlin/com/akoolla/adventofcode/day001.kt
@@ -1,46 +1,92 @@
 package com.akoolla.adventofcode
 
+import io.restassured.internal.common.assertion.Assertion
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.io.File
 
 val calibrationDocContents = listOf(
-    "1abc2",
-    "pqr3stu8vwx",
-    "a1b2c3d4e5f",
-    "treb7uchet",
+    "two1nine",
+    "eightwothree",
+    "abcone2threexyz",
+    "xtwone3four",
+    "4nineeightseven2",
+    "zoneight234",
+    "7pqrstsixteen",
+)
+
+fun String.findAndReplaceSpelledDigits() = this
+    .toList()
+    .map { it.toString() }
+    .reduce { acc, s ->
+        var line: String = acc + s
+        textToDigitDictionary
+            .keys
+            .firstOrNull {
+                //println(it)
+                line.contains(it)
+            }
+            ?.run {
+                line = line.replace(this.substring(0, this.length -1), textToDigitDictionary[this] ?: "x")
+            }
+        line
+    }
+
+val textToDigitDictionary = mapOf(
+    Pair("one", "1"),
+    Pair("two", "2"),
+    Pair("six", "6"),
+    Pair("four", "4"),
+    Pair("five", "5"),
+    Pair("nine", "9"),
+    Pair("three", "3"),
+    Pair("seven", "7"),
+    Pair("eight", "8"),
 )
 
 fun List<String>.decodeAndSum(): Int = this
+//    .map { it.findAndReplaceSpelledDigits() }
     .sumOf { it.getDigitsFromLine() }
 
 fun String.getDigitsFromLine(): Int = this
+    .findAndReplaceSpelledDigits()
     .filter { it.isDigit() }
     .toList()
     .run { "" + this.first() + this.last() }
-    .run { Integer.parseInt(this)}
+    .run { Integer.parseInt(this) }
 
 class CalibrationDecoderTests {
+    @Test
+    fun canFindAndReplaceTextDigitsWithDigits() {
+        Assertions.assertEquals(18, "oneeight".getDigitsFromLine())
+    }
 
     @Test
-    fun canGetDigitsFromLine(){
+    fun canGetDigitsFromLine() {
         Assertions.assertEquals(12, "1abc2".getDigitsFromLine())
         Assertions.assertEquals(38, "pqr3stu8vwx".getDigitsFromLine())
         Assertions.assertEquals(15, "a1b2c3d4e5f".getDigitsFromLine())
         Assertions.assertEquals(77, "treb7uchet".getDigitsFromLine())
+        Assertions.assertEquals(29, "two1nine".getDigitsFromLine())
+        Assertions.assertEquals(29, "two1nine".getDigitsFromLine())
+        Assertions.assertEquals(83, "eightwothree".getDigitsFromLine())
+        Assertions.assertEquals(13, "abcone2threexyz".getDigitsFromLine())
+        Assertions.assertEquals(24, "xtwone3four".getDigitsFromLine())
+        Assertions.assertEquals(14, "zoneight234".getDigitsFromLine())
+        Assertions.assertEquals(76, "7pqrstsixteen".getDigitsFromLine())
     }
 
     @Test
-    fun canSumDigitsFromMultipleLines(){
-        Assertions.assertEquals(142, calibrationDocContents.decodeAndSum())
+    fun canSumDigitsFromMultipleLines() {
+        Assertions.assertEquals(281, calibrationDocContents.decodeAndSum())
     }
 
     @Test
-    fun canDecodeAndSumDay001File(){
+    fun canDecodeAndSumDay001File() {
         val calibrationValue: Int = File("src/test/resources/day001Input.txt")
-                .useLines { it.toList() }
-                .decodeAndSum()
+            .useLines { it.toList() }
+            .decodeAndSum()
 
-        Assertions.assertEquals(56049, calibrationValue)
+        Assertions.assertEquals(54530, calibrationValue)
     }
 }
